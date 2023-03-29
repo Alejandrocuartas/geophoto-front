89c85b79f3c66c8be4955fb984fc15323d25eb70
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-03-29 13:37:52 -0500

    config base

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,20 @@
-console.log("HI")
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import App from "./App";
+import { Context } from "./context";
+
+const client = new ApolloClient({
+    uri: `/graphql`,
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Context>
+            <App />
+        </Context>
+    </ApolloProvider>,
+    document.getElementById("root")
+);
