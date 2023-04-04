187d7bfd0bcb052795dbb78d93f7af80158e27f9
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-04-04 14:13:13 -0500

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { BiImageAdd } from 'react-icons/Bi'
+import { BiImageAdd } from 'react-icons/bi'
 import { useGlobalState } from '../context';
 
 import './styles/Navbar.css'
