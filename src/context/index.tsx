de7ca5d76a9208f383cabf6c86c14ccfa5a6fc9e
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-04-04 14:19:27 -0500

    sockets secure

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext, useContext } from "react";
 import { GlobalContext, Photo } from "../types";
-const socket = new WebSocket(`ws://${process.env.API}/ws`)
+const socket = new WebSocket(`wss://${process.env.API}/ws`)
 //@ts-ignore
 const logContext = createContext<GlobalContext>();
 const Context = ({ children }: { children: JSX.Element }) => {
